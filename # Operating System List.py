2547e20d5446d4f1418781f717a400d327d1af49
Eduardo Sayans <49099715+SayansED@users.noreply.github.com>
2021-07-09 18:19:43 -0300

    up

---
+++ # Operating System List.py
@@ -0,0 +1,16 @@
+List = ['C#', 'C++', 'C']
+List.reverse()
+print(List)
+
+a, b = 2021, 2020
+a, b = b, a
+print(a)
+print(b)
+
+sum(i for i in range(10))
+
+import time
+
+print("Output imediato.")
+time.sleep(10.5)
+print("Output depois de 10.5 segundos.")
